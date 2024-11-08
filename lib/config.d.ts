9a1b510b7a3e0c8303f2c5507a25ca7d4706f8d6
TR0MXI <ytang7543@gmail.com>
2024-11-08 21:34:33 +0800

    chore: delete lib

--- lib/config.d.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "koishi";
-export interface Config {
-    path: string;
-    token: string;
-    ping: number;
-    timeout: number;
-    debug: boolean;
-}
-export declare const Config: Schema<Config>;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Context } from 'koishi';
-import { Config } from './config';
-export declare const name = "haluna";
-export * from './config';
-export declare function apply(ctx: Context, config: Config): Promise<void>;
